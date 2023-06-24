279dc3424f265487e061ff3cb899bc02d9343913
MAUSHAMRAJ <rajmausham970@gmail.com>
2023-06-24 21:58:34 +0530

    fixed experience

--- src/constants/index.js
+++ src/constants/index.js
@@ -113,7 +113,7 @@ const experiences = [
     company_name: "Spark-Eighteen",
     icon: sparkeighteenlogo,
     iconBg: "#E6DEDD",
-    date: "June 2022 -April 2023",
+    date: "June 2022 -Jan 2023",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
